329d16ee6063981e37aee14617f65083e333c9bf
mynulyt <mynulalam27@gmail.com>
2025-12-17 22:20:25 +0600

    localization

---
+++ lib/app/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CraftyBuyApp extends StatefulWidget {
+  const CraftyBuyApp({super.key});
+
+  @override
+  State<CraftyBuyApp> createState() => _CraftyBuyAppState();
+}
+
+class _CraftyBuyAppState extends State<CraftyBuyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(onGenerateRoute: (settings) {});
+  }
+}
